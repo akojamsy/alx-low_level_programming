d93e3794918d5055dff160ef08ddced9168e7610
akojamsy <akoguitar@gmail.com>
2022-03-19 01:25:54 +0100

    0-isupper.c

---
+++ 0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase
+ * @c: is the argument
+ *
+ * Return: 1 when uppercase
+ * 0 otherwise
+*/
+
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
